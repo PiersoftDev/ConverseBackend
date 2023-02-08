8901d62ad0f5d1370b2b8e6c0df71c9133404a7b
91994 <mahendra.cp1988@gmail.com>
2023-02-08 09:32:53 +0530

    initial commit

---
+++ src/main/java/com/piersoft/converse/vendor/ConverseVendorManagementApplication.java
@@ -0,0 +1,13 @@
+package com.piersoft.converse.vendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverseVendorManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverseVendorManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piersoft/converse/vendor/ConverseVendorManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piersoft.converse.vendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverseVendorManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
